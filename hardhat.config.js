fdcc36a5c373445dfce618e6ca2be010dd2e4112
ibrahim kenia <ibrahimkenia2@gmail.com>
2022-08-23 22:55:30 +0100

    Bank contract

---
+++ hardhat.config.js
@@ -0,0 +1,13 @@
+
+require('@openzeppelin/hardhat-upgrades');
+require("@nomiclabs/hardhat-waffle");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version : "0.8.0"
+  },
+  paths:{
+    artifacts : "./client/src/artifacts",
+  }
+};
